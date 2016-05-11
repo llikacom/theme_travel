349e2cd5887fd48ec69c179f9ca6bff69f5f90a4
j114143 <hm.erajuan@gmail.com>
2016-05-11 18:55:09 -0500

    hook para menu arbolito

--- template.php
+++ template.php
@@ -6,3 +6,11 @@ function theme_travel_preprocess_page(&$vars)
         $vars['theme_hook_suggestions'][] = 'page__' . $nodetype;
     }
 }
+function theme_travel_links($variables) {
+    if (array_key_exists('id', $variables['attributes']) && $variables['attributes']['id'] == 'main-menu-links') {
+        $pid = variable_get('menu_main_links_source', 'main-menu');
+    $tree = menu_tree($pid);
+    return drupal_render($tree);
+    }
+    return theme_links($variables);
+}
