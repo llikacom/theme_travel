18e5f824649601b5b15a9f2ced1e0fc4eea62aa2
j114143 <hm.erajuan@gmail.com>
2016-05-11 18:44:25 -0500

    viewport to head

--- html.tpl.php
+++ html.tpl.php
@@ -3,6 +3,9 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="<?php print $language->language; ?>" version="XHTML+RDFa 1.0" dir="<?php print $language->dir; ?>"<?php print $rdf_namespaces; ?>>
 
 <head profile="<?php print $grddl_profile; ?>">
+  <meta name="googlebot" content="noindex">
+  <meta name="robots" content="noindex">
+  <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=2, user-scalable=1">
   <?php print $head; ?>
   <title><?php print $head_title; ?></title>
   <?php print $styles; ?>
