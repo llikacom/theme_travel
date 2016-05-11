d24768935c7012bc88ad4e57dd8890aa16039111
j114143 <hm.erajuan@gmail.com>
2016-05-11 16:03:37 -0500

    hook preporcess para plantillas

--- template.php
+++ template.php
@@ -1,1 +1,8 @@
 <?php
+function theme_travel_preprocess_page(&$vars)
+{
+    if (isset($vars['node']->type)) {
+        $nodetype = $vars['node']->type;
+        $vars['theme_hook_suggestions'][] = 'page__' . $nodetype;
+    }
+}
