d204b615d0b64ccea16a6ccee5df6a5269a46864
Juan Eracleo Huamani Mendoza <hm.erajuan@gmail.com>
2016-12-07 15:18:14 -0500

    Create node--slider.tpl.php
    Slider template

---
+++ node--slider.tpl.php
@@ -0,0 +1,8 @@
+<div class="zone-slider">
+    <?php print render($content['field_image']); ?>
+    <div class="container text-right slider-content">
+        <h3><?php print $title; ?></h3>
+        <?php print render($content['body']); ?>
+        <?php print render($content['field_link']); ?>
+    </div>
+</div>
