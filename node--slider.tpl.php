354baf5ce9b4ab7d9fafb990b78b92e8df520c90
Juan Eracleo Huamani Mendoza <hm.erajuan@gmail.com>
2016-12-07 16:12:06 -0500

    Custom class
    custom class added

--- node--slider.tpl.php
+++ node--slider.tpl.php
@@ -1,8 +1,15 @@
 <div class="zone-slider">
     <?php print render($content['field_image']); ?>
-    <div class="container text-right slider-content">
-        <h3><?php print $title; ?></h3>
-        <?php print render($content['body']); ?>
-        <?php print render($content['field_link']); ?>
+    <div class="slider-content">
+      <div class="container">
+        <div class="row">
+          <div class="col-md-7 col-lg-8"></div>
+          <div class="col-md-5 col-lg-4">
+          <h3><?php print $title; ?></h3>
+          <?php print render($content['body']); ?>
+          <?php print render($content['field_link']); ?>
+          </div>
+        </div>
+      </div>
     </div>
 </div>
