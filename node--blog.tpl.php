8085342f694d5a7452cf2be279953402776648e2
Juan Eracleo Huamani Mendoza <hm.erajuan@gmail.com>
2016-12-12 13:47:41 -0500

    teaser container, bootstraped

--- node--blog.tpl.php
+++ node--blog.tpl.php
@@ -8,16 +8,18 @@
   </div>
 </div>
 <?php else: ?>
-<div class="row blog-teaser" id="node-<?php print $node->nid; ?>">
-  <div class="col-sm-4">
-  <?php
-  print render($content['field_image']);
-  ?>
-  </div>
-  <div class="col-sm-8">
-    <h2><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
-    <?php print render($content['body']); ?>
-    <p class="read-more text-right"><a href="<?php print $node_url; ?>" class="btn-more"><?php print t("Read more") ; ?></a></p>
+<div class="blog-teaser">
+  <div class="row">
+    <div class="col-sm-4">
+    <?php
+    print render($content['field_image']);
+    ?>
+    </div>
+    <div class="col-sm-8">
+      <h2><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+      <?php print render($content['body']); ?>
+      <p class="text-right read-more"><a href="<?php print $node_url; ?>" class="btn btn-more"><?php print t("Read more") ; ?></a></p>
+    </div>
   </div>
 </div>
 <?php endif; ?>
