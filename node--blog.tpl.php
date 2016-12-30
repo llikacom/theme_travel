70421f05a495af8af228531556ad61e293029830
Juan Eracleo Huamani Mendoza <juan@escodigo.com>
2016-12-30 17:38:20 -0500

    nombres de clase cambiados

--- node--blog.tpl.php
+++ node--blog.tpl.php
@@ -1,5 +1,5 @@
 <?php if ($page): ?>
-<div class="blog-full">
+<div class="node-full blog-full">
   <div class="content">
     <?php
       hide($content['links']);
@@ -8,17 +8,15 @@
   </div>
 </div>
 <?php else: ?>
-<div class="blog-teaser">
+<div class="node-teaser blog-teaser">
   <div class="row">
     <div class="col-sm-4">
-    <?php
-    print render($content['field_image']);
-    ?>
+    <?php print render($content['field_image']); ?>
     </div>
     <div class="col-sm-8">
-      <h2><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h2>
+      <h2><a href="<?php print $node_url; ?>" title="<?php print $title; ?>"><?php print $title; ?></a></h2>
       <?php print render($content['body']); ?>
-      <p class="text-right read-more"><a href="<?php print $node_url; ?>" class="btn btn-more"><?php print t("Read more") ; ?></a></p>
+      <p class="text-right read-more"><a href="<?php print $node_url; ?>" class="btn btn-primary btn-more"><?php print t("Read more") ; ?></a></p>
     </div>
   </div>
 </div>
