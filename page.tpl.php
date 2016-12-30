a8ef8b24b067c22ed749b26d9a34d12719e40f45
Juan Eracleo Huamani Mendoza <juan@escodigo.com>
2016-12-30 16:22:25 -0500

    Titulo solo en h1

--- page.tpl.php
+++ page.tpl.php
@@ -36,7 +36,7 @@
 - Insertar titulo, tabs, contenido
 <?php print render($title_prefix); ?>
 <?php if ($title): ?>
-<h1 class="title"><span class="line-center"><?php print $title; ?></span></h1>
+<h1 class="title"><?php print $title; ?></h1>
 <?php endif; ?>
 <?php print render($title_suffix); ?>
 <?php if ($tabs): ?>
