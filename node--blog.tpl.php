51f91e5a4cf39c7051931d38541391835ff6adf7
Juan Eracleo Huamani Mendoza <juan@escodigo.com>
2017-01-02 18:45:36 -0500

    update node--blog.tpl.php
    Link como boton primario

--- node--blog.tpl.php
+++ node--blog.tpl.php
@@ -16,7 +16,7 @@
     <div class="col-sm-8">
       <h2><a href="<?php print $node_url; ?>" title="<?php print $title; ?>"><?php print $title; ?></a></h2>
       <?php print render($content['body']); ?>
-      <p class="text-right read-more"><a href="<?php print $node_url; ?>" class="btn btn-primary btn-more"><?php print t("Read more") ; ?></a></p>
+      <p class="text-right read-more"><a href="<?php print $node_url; ?>" class="btn btn-primary btn-primary btn-more"><?php print t("Read more") ; ?></a></p>
     </div>
   </div>
 </div>
