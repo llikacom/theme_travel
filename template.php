56c1952a95ab3208b5ba824180a3fbd84ce16083
Juan Eracleo Huamani Mendoza <juan@escodigo.com>
2017-01-18 11:50:07 -0500

    template.tpl.php
    Insertar viewport en el tpl.php

--- template.php
+++ template.php
@@ -14,3 +14,14 @@ function theme_travel_links($variables) {
     }
     return theme_links($variables);
 }
+
+function theme_travel_preprocess_html(&$vars) {
+    $viewport = array(
+      '#tag' => 'meta', 
+      '#attributes' => array(
+        'name' => 'viewport', 
+        'content' => 'width=device-width, initial-scale=1, maximum-scale=2, user-scalable=1',
+      ),
+    ); 
+    drupal_add_html_head($viewport, 'viewport');
+}
