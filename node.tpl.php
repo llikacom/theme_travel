c973666b6d3576e59e622ecbad2f33cd920c038e
Juan Eracleo Huamani Mendoza <juan@escodigo.com>
2017-01-25 12:40:53 -0500

    Node fixed
    Node estructure fixed

--- node.tpl.php
+++ node.tpl.php
@@ -1,21 +1,23 @@
 <?php if ($page): ?>
 <div class="node-full <?php print $type; ?>-full">
-- Renderizar campo
-<?php print render($content['field_name']); ?>
-- Tenderizar Titulo del campo
-<?php if(array_key_exists('field_itinerary',$content)): ?>
-<?php print $content['field_itinerary']["#title"]; ?></li>
-<?php endif; ?>
-- Renderizar campo como boton, un campo de node reference
-<a href="<?php print $content['field_book_now']['0']['#markup']; ?>?tour=<?php print $title; ?>" class="btn btn-primary btn-book" title="<?php print $content['field_book_now']['#title']; ?>">
-<?php print $content['field_book_now']['#title']; ?>
-</a>
+  <div class="content">
+    <?php
+      hide($content['links']);
+      print render($content);
+    ?>
+  </div>
 </div>
 <?php else: ?>
 <div class="node-teaser <?php print $type; ?>-teaser">
-  <h3 class="text-center"><a href="<?php print $node_url; ?>"><?php print $title; ?></a></h3>
-  <p class="text-center">
-      <a href="<?php print $node_url; ?>" class="btn btn-primary btn-more text-upper"><?php print t("VIEW MORE"); ?></a>
-    </p>
+  <div class="row">
+    <div class="col-sm-4">
+    <?php print render($content['field_image']); ?>
+    </div>
+    <div class="col-sm-8">
+      <h2 class="title"><a href="<?php print $node_url; ?>" title="<?php print $title; ?>"><?php print $title; ?></a></h2>
+      <?php print render($content['body']); ?>
+      <p class="text-right read-more"><a href="<?php print $node_url; ?>" class="btn btn-primary btn-primary btn-more"><?php print t("Read more") ; ?></a></p>
+    </div>
+  </div>
 </div>
-<?php endif; ?>
+<?php endif; ?> 
